4574dcf18a2292e01bf9b59e18a1f195cbb25b6e
anvyidol <phanleho002@gmail.com>
2023-03-04 13:57:19 +0700

    update

--- OUNet Management Application/Forms/FrmStatistical.Designer.cs
+++ OUNet Management Application/Forms/FrmStatistical.Designer.cs
@@ -28,20 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lwDataList = new System.Windows.Forms.ListView();
+            this.time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lbTo = new System.Windows.Forms.Label();
+            this.lbFrom = new System.Windows.Forms.Label();
+            this.pbSearch = new System.Windows.Forms.PictureBox();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.service = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.money = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbSum = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSearch)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.lbTo);
+            this.panel1.Controls.Add(this.lbFrom);
+            this.panel1.Controls.Add(this.pbSearch);
+            this.panel1.Controls.Add(this.dtpTo);
+            this.panel1.Controls.Add(this.dtpFrom);
+            this.panel1.Controls.Add(this.lwDataList);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1478, 759);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lwDataList
+            // 
+            this.lwDataList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lwDataList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.time,
+            this.service,
+            this.money,
+            this.sum});
+            this.lwDataList.HideSelection = false;
+            this.lwDataList.Location = new System.Drawing.Point(16, 74);
+            this.lwDataList.Name = "lwDataList";
+            this.lwDataList.Size = new System.Drawing.Size(1450, 608);
+            this.lwDataList.TabIndex = 13;
+            this.lwDataList.UseCompatibleStateImageBehavior = false;
+            this.lwDataList.View = System.Windows.Forms.View.Details;
+            // 
+            // time
+            // 
+            this.time.Text = "Thời gian";
+            this.time.Width = 355;
+            // 
+            // lbTo
+            // 
+            this.lbTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTo.AutoSize = true;
+            this.lbTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTo.Location = new System.Drawing.Point(1142, 20);
+            this.lbTo.Name = "lbTo";
+            this.lbTo.Size = new System.Drawing.Size(48, 20);
+            this.lbTo.TabIndex = 25;
+            this.lbTo.Text = "Đến:";
+            this.lbTo.Click += new System.EventHandler(this.lbTo_Click);
+            // 
+            // lbFrom
+            // 
+            this.lbFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbFrom.AutoSize = true;
+            this.lbFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFrom.Location = new System.Drawing.Point(858, 20);
+            this.lbFrom.Name = "lbFrom";
+            this.lbFrom.Size = new System.Drawing.Size(36, 20);
+            this.lbFrom.TabIndex = 24;
+            this.lbFrom.Text = "Từ:";
+            this.lbFrom.Click += new System.EventHandler(this.lbFrom_Click);
+            // 
+            // pbSearch
+            // 
+            this.pbSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbSearch.BackColor = System.Drawing.Color.IndianRed;
+            this.pbSearch.Image = global::OUNet_Management_Application.Properties.Resources.Search_Icon;
+            this.pbSearch.Location = new System.Drawing.Point(1425, 13);
+            this.pbSearch.Name = "pbSearch";
+            this.pbSearch.Padding = new System.Windows.Forms.Padding(8, 4, 8, 4);
+            this.pbSearch.Size = new System.Drawing.Size(41, 33);
+            this.pbSearch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbSearch.TabIndex = 23;
+            this.pbSearch.TabStop = false;
+            this.pbSearch.Click += new System.EventHandler(this.pbSearch_Click);
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.AllowDrop = true;
+            this.dtpTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpTo.CustomFormat = "MM - dd -  yyyy | hh:mm";
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTo.Location = new System.Drawing.Point(1200, 19);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(200, 22);
+            this.dtpTo.TabIndex = 22;
+            this.dtpTo.ValueChanged += new System.EventHandler(this.dtpTo_ValueChanged);
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.AllowDrop = true;
+            this.dtpFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpFrom.CustomFormat = "MM - dd -  yyyy | hh:mm";
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFrom.Location = new System.Drawing.Point(904, 19);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(200, 22);
+            this.dtpFrom.TabIndex = 21;
+            this.dtpFrom.ValueChanged += new System.EventHandler(this.dtpFrom_ValueChanged);
+            // 
+            // service
+            // 
+            this.service.Text = "Ăn uống";
+            this.service.Width = 362;
+            // 
+            // money
+            // 
+            this.money.Text = "Nạp tiền";
+            this.money.Width = 362;
+            // 
+            // sum
+            // 
+            this.sum.Text = "Tổng doanh thu";
+            this.sum.Width = 363;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Silver;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.lbSum, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 699);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1450, 47);
+            this.tableLayoutPanel1.TabIndex = 27;
+            // 
+            // lbSum
+            // 
+            this.lbSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSum.Location = new System.Drawing.Point(15, 14);
+            this.lbSum.Name = "lbSum";
+            this.lbSum.Size = new System.Drawing.Size(103, 24);
+            this.lbSum.TabIndex = 0;
+            this.lbSum.Text = "Tổng cộng";
             // 
             // FrmStatistical
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(1502, 783);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmStatistical";
             this.Text = "FrmStatistical";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSearch)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView lwDataList;
+        private System.Windows.Forms.ColumnHeader time;
+        private System.Windows.Forms.Label lbTo;
+        private System.Windows.Forms.Label lbFrom;
+        private System.Windows.Forms.PictureBox pbSearch;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.ColumnHeader service;
+        private System.Windows.Forms.ColumnHeader money;
+        private System.Windows.Forms.ColumnHeader sum;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lbSum;
     }
 }
